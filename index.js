b8a6ae1c796dab57b23e0dabd5558bbe5e7ef64c
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-10-02 22:24:47 +0200

    Create Express.js server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello from Node.js!')
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Serwer nasłuchuje na porcie ${PORT}`);
+});
