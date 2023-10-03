e33496877e9c4186d76b361fd48469c9172c8354
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-10-03 10:13:58 +0200

    Mongo conncet

--- index.ts
+++ index.ts
@@ -1,5 +1,29 @@
 import { Express, Request, Response } from "express";
 
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://CharlesSquirel:${process.env.APP_MONGOPASSWORD}@fishkappapi.koexjkr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 const express = require("express");
 const app: Express = express();
 
