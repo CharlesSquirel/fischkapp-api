c6221204051cd3971e9d1e17ea23c8c6696d44c1
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-10-28 18:53:19 +0200

    Moongose connection in tests

--- src/_tests_/App.test.ts
+++ src/_tests_/App.test.ts
@@ -1,3 +1,16 @@
 const request = require("supertest");
-
 const app = require("./app");
+const mongoose = require("mongoose");
+require("dotenv").config();
+import env from "../../util/validateEnv";
+
+const port = env.PORT;
+const mongoStr = env.MONGODB_CONNECTION_STRING;
+
+beforeEach(async () => {
+  await mongoose.connect(mongoStr);
+});
+
+afterEach(async () => {
+  await mongoose.connection.close();
+});
