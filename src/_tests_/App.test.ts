a3cb492ce609710fd8907fc220eb808551020510
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-10-29 16:18:15 +0100

    test GET /cards

--- src/_tests_/App.test.ts
+++ src/_tests_/App.test.ts
@@ -1,12 +1,18 @@
-const request = require("supertest");
+import { MongoMemoryServer } from "mongodb-memory-server";
 import app from "../app";
-const mongoose = require("mongoose");
 import env from "../../util/validateEnv";
+const request = require("supertest");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 require("dotenv").config();
+import CardModel from "../models/model";
 
 const auth = env.AUTHORIZATION_STRING;
 
-import { MongoMemoryServer } from "mongodb-memory-server";
+const getCountFromMongoDB = async () => {
+  const count = await CardModel.countDocuments();
+  return count;
+};
 
 let mongoServer: MongoMemoryServer;
 
@@ -21,9 +27,18 @@ afterAll(async () => {
   await mongoServer.stop();
 });
 
-describe("GET /cards", () => {
-  it("fetch all cards", async () => {
+describe("Test finding cards", () => {
+  it("GET /cards", async () => {
     const response = await request(app).get("/cards").set("Authorization", `Bearer ${auth}`);
     expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    const isDescending = _.isEqual(response.body, _.orderBy(response.body, [], ["desc"]));
+    expect(isDescending).toBe(true);
+    const expectedCount = await getCountFromMongoDB();
+    expect(response.body.length).toBe(expectedCount);
+  });
+
+  it("GET /cards/author/:author", async () => {
+    const response = await request(app).get("/cards/author").set("Authorization", `Bearer ${auth}`);
   });
 });
