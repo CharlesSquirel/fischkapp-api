7cdbe3ad56fef388f73801c6792dd9fb63062e6e
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-11-01 22:40:16 +0100

    Mock testing

--- src/_tests_/App.test.ts
+++ src/_tests_/App.test.ts
@@ -9,6 +9,22 @@ import CardModel from "../models/model";
 
 const auth = env.AUTHORIZATION_STRING;
 
+const mockedPOST = [
+  {
+    front: "test1",
+    back: "test1",
+    tags: ["test1"],
+    author: "test author",
+  },
+  {
+    front: "test2",
+    back: "test2",
+    tags: ["test2"],
+    author: "test author",
+  },
+  { front: "test3", back: "test3", tags: ["test3"], author: "test author" },
+];
+
 const getCountFromMongoDB = async () => {
   const count = await CardModel.countDocuments();
   return count;
@@ -39,6 +55,7 @@ describe("Test finding cards", () => {
   });
 
   it("GET /cards/author/:author", async () => {
+    await CardModel.create(mockedPOST);
     const response = await request(app).get("/cards/author").set("Authorization", `Bearer ${auth}`);
   });
 });
